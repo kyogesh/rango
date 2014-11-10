a889353a97a31745ec82a90aaaebadb0795ea4d7
Yogesh Kumar <yk.301189@gmail.com>
2014-11-10 16:43:08 +0530

    Added Model form for Person.

--- rango/forms.py
+++ rango/forms.py
@@ -1,14 +1,27 @@
 from django import forms
-from .models import Page, Category
+from .models import Page, Category, RangoUser
+from django.contrib.auth.models import User
 
 class PageForm(forms.ModelForm):
 
     class Meta:
         model = Page
-        fields = ('title', 'body', 'category',)
+        fields = ('title', 'body', 'category')
 
 class CategoryForm(forms.ModelForm):
 
     class Meta:
         model = Category
-        fields = ('name')
+        fields = ('name',)
+
+class RangoUserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class RangoUserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = RangoUser
+        fields = ('website', 'profile_pic')
